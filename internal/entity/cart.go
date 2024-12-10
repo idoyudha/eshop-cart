d69cc54b0ba15f0205cbdc2c4a7ea2c573e49620
idoyudha <idowidya.yudhatama@gmail.com>
2024-12-10 22:16:20 +0700

    feat: cart redis repo

--- internal/entity/cart.go
+++ internal/entity/cart.go
@@ -7,14 +7,14 @@ import (
 )
 
 type Cart struct {
-	ID              uuid.UUID  `json:"id"`
-	UserID          uuid.UUID  `json:"user_id"`
-	ProductID       uuid.UUID  `json:"product_id"`
-	ProductName     string     `json:"product_name"`
-	ProductPrice    float64    `json:"product_price"`
-	ProductQuantity int64      `json:"product_quantity"`
-	Note            *string    `json:"note,omitempty"`
-	CreatedAt       time.Time  `json:"created_at"`
-	UpdatedAt       time.Time  `json:"updated_at"`
-	DeletedAt       *time.Time `json:"deleted_at,omitempty"`
+	ID              uuid.UUID `json:"id"`
+	UserID          uuid.UUID `json:"user_id"`
+	ProductID       uuid.UUID `json:"product_id"`
+	ProductName     string    `json:"product_name"`
+	ProductPrice    float64   `json:"product_price"`
+	ProductQuantity int64     `json:"product_quantity"`
+	Note            string    `json:"note"`
+	CreatedAt       time.Time `json:"created_at"`
+	UpdatedAt       time.Time `json:"updated_at"`
+	DeletedAt       time.Time `json:"deleted_at"`
 }
--- internal/usecase/cart.go
+++ internal/usecase/cart.go
@@ -3,6 +3,7 @@ package usecase
 import (
 	"context"
 
+	"github.com/google/uuid"
 	"github.com/idoyudha/eshop-cart/internal/entity"
 )
 
@@ -10,20 +11,20 @@ type (
 	CartMySQLRepo interface {
 		Insert(context.Context, *entity.Cart) error
 		Update(context.Context, *entity.Cart) error
-		Get(context.Context, int64) (*entity.Cart, error)
-		Delete(context.Context, int64) error
+		Get(context.Context, uuid.UUID) (*entity.Cart, error)
+		Delete(context.Context, uuid.UUID) error
 	}
 
 	CartRedisRepo interface {
 		Save(context.Context, *entity.Cart) error
-		Get(context.Context, int64) (*entity.Cart, error)
-		Delete(context.Context, int64) error
+		GetUserCart(context.Context, string) (*entity.Cart, error)
+		DeleteCarts(context.Context, string, []string) error
 	}
 
 	Cart interface {
 		CreateCart(context.Context, *entity.Cart) error
-		GetCart(context.Context, int64) (*entity.Cart, error)
+		GetUserCart(context.Context, string) ([]*entity.Cart, error)
 		UpdateCart(context.Context, *entity.Cart) error
-		DeleteCart(context.Context, int64) error
+		DeleteCarts(context.Context, uuid.UUID, uuid.UUIDs) error
 	}
 )
