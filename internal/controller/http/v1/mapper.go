9d034bb2923b17d11a28f659422eea0c7da2b870
idoyudha <idowidya.yudhatama@gmail.com>
2024-12-12 20:20:47 +0700

    feat: cart controller and mapper

---
+++ internal/controller/http/v1/mapper.go
@@ -0,0 +1,36 @@
+package v1
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/idoyudha/eshop-cart/internal/entity"
+)
+
+func CreateCartRequestToCartEntity(userID uuid.UUID, req CreateCartRequest) (entity.Cart, error) {
+	cartID, err := uuid.NewV7()
+	if err != nil {
+		return entity.Cart{}, err
+	}
+	return entity.Cart{
+		ID:              cartID,
+		UserID:          userID,
+		ProductID:       req.ProductID,
+		ProductName:     req.ProductName,
+		ProductPrice:    req.ProductPrice,
+		ProductQuantity: req.ProductQuantity,
+		Note:            req.Note,
+		CreatedAt:       time.Now(),
+		UpdatedAt:       time.Now(),
+	}, nil
+}
+
+func UpdateCartRequestToCartEntity(cartID uuid.UUID, userID uuid.UUID, req UpdateCartRequest) entity.Cart {
+	return entity.Cart{
+		ID:              cartID,
+		UserID:          userID,
+		ProductQuantity: req.ProductQuantity,
+		Note:            req.Note,
+		UpdatedAt:       time.Now(),
+	}
+}
